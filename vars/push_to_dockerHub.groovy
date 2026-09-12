25aa089b9604fee0a7378f9d7d8860db75403bc9
vsolanki7779 <vsolanki7779@gmail.com>
2026-09-12 23:06:17 +0530

    Update push_to_dockerHub.groovy

--- vars/push_to_dockerHub.groovy
+++ vars/push_to_dockerHub.groovy
@@ -1,2 +1,10 @@
-def call(){
+def call(String Image_name , String Tag_name){
+    echo "Pushing Image to DockerHub"
+    withCredentials([usernamePassword('credentialsId':'docker-hub-cred',
+    passwordVariable:'dockerHubPass',
+    usernameVariable:"dockerHubUser")]){
+    sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
+    sh "docker image tag ${Image_name}:${Tag_name} ${env.dockerHubUser}/${Image_name}:${Tag_name}"
+    sh "docker push ${env.dockerHubUser}/${Image_name}:${Tag_name}"}
 }
+
